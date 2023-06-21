eaaeae057b811adbd41deeddc8372b5e9548580d
Partha Sai <sparthasai@gmail.com>
2023-06-21 21:56:25 +0530

    edited plugin_dir extraction to avoid junk string

--- install.sh
+++ install.sh
@@ -7,16 +7,16 @@ if [[ $# > 0 ]]; then
     fi
 
     sql_result=$(mysql --user=$1 --password=$2 -s -N -e "SHOW VARIABLES LIKE 'plugin_dir';")
-    plugin_dir=$(cut -d" " -f2 <<< $sql_result)
+    plugin_dir=$(awk '{print $2}' <<< "$sql_result")
 
     export CGO_CFLAGS=$include_dir
     go build -buildmode=c-shared -o $plugin_dir"http.so" http.go
     rm $plugin_dir"http.h"
 
-    mysql --user=$1 --password=$2 -s -N -e "CREATE OR REPLACE FUNCTION http_help RETURNS STRING SONAME 'http.so';"
-    mysql --user=$1 --password=$2 -s -N -e "CREATE OR REPLACE FUNCTION http_raw RETURNS STRING SONAME 'http.so';"
-    mysql --user=$1 --password=$2 -s -N -e "CREATE OR REPLACE FUNCTION http_get RETURNS STRING SONAME 'http.so';"
-    mysql --user=$1 --password=$2 -s -N -e "CREATE OR REPLACE FUNCTION http_post RETURNS STRING SONAME 'http.so';"
+    mysql --user=$1 --password=$2 -s -N -e "CREATE FUNCTION http_help RETURNS STRING SONAME 'http.so';"
+    mysql --user=$1 --password=$2 -s -N -e "CREATE FUNCTION http_raw RETURNS STRING SONAME 'http.so';"
+    mysql --user=$1 --password=$2 -s -N -e "CREATE FUNCTION http_get RETURNS STRING SONAME 'http.so';"
+    mysql --user=$1 --password=$2 -s -N -e "CREATE FUNCTION http_post RETURNS STRING SONAME 'http.so';"
 
     echo "Install Success"
 else
